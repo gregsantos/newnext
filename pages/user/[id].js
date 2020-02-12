af0c638a2c21bb6c77248b38c2112f64d5a6f540
Greg Santos <gregsantos@gmail.com>
2020-02-11 16:16:52 -0800

    Update users fetch uri

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -5,7 +5,7 @@ const User = ({ user }) => <div>{user.name}</div>
 User.getInitialProps = async ({ query: { id } }, res) => {
   const protocol = req ? req.headers['x-forwarded-proto'] || 'http' : ''
   const baseUrl = req ? `${protocol}://${req.headers.host}` : ''
-  const response = await fetch(`${baseUrl + '/api/user/' + ${id}}`)
+  const response = await fetch(`${baseUrl + '/api/user/' + id}`)
   //  const response = await fetch(`http://localhost:3000/api/user/${id}`)
   const user = await response.json()
 
